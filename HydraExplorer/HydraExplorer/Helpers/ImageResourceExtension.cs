166fc6f01811dac5cfaaa08b8a7dbc192d9c4186
eddyb <eddyboughioul@hotmail.fr>
2021-10-22 08:35:37 +0200

    add missing file

---
+++ HydraExplorer/HydraExplorer/Helpers/ImageResourceExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HydraExplorer.Helpers
+{
+
+    [ContentProperty(nameof(Source))]
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+            {
+                return null;
+            }
+            else
+            {
+                Source = "HydraExplorer.Resources.Images." + Source;
+            }
+
+            // Do your translation lookup here, using whatever method you require
+            var imageSource = ImageSource.FromResource(Source, typeof(ImageResourceExtension).GetTypeInfo().Assembly);
+
+            return imageSource;
+        }
+    }
+}
