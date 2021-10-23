5c4c3093109b72427981e02ae71981b88932f0a4
Eddy Boughioul <eddyboughioul@hotmail.fr>
2021-10-23 19:00:45 +0200

    Update style

---
+++ HydraExplorer/HydraExplorer/Helpers/NullOrEmptyBooleanConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms;
+
+namespace HydraExplorer.Helpers
+{
+    public class NullOrEmptyBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value?.ToString() != parameter?.ToString();
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
